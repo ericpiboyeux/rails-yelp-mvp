d5403d99bac3d2d15a83b31b7939dbe041b60f5c
Eric Piboyeux <ericpiboyeux@gmail.com>
2017-02-15 12:50:06 +0100

    Restaurant and Review models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,13 @@
+class Restaurant < ApplicationRecord
+  @categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :adress, presence: true
+  validates :category, inclusion: { in: @categories, allow_nil: true }
+
+  def self.categories
+    @categories
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "La bonne fourchette",
+    adress:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone:        "01 12 23 15 15",
+    category:     "french"
+  },
+  {
+    name:         "Le Brussels",
+    adress:      "4 rue Blainville 75005 Paris",
+    phone:        "01 45 12 20 20",
+    category:     "belgian"
+  },
+  {
+    name:         "Chez Maxou",
+    adress:      "route de Suresnes 75016 Paris",
+    phone:        "01 64 32 77 77",
+    category:     "french"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
